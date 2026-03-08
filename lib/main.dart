f5d7bef22e588bbe910001db123ba5a73bbc4647
markvanmeerten <markvanmeerten@gmail.com>
2026-03-08 18:57:24 +0100

    remove test code

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todo/pages/task_home_page.dart';
-import 'package:todo/models/task.dart';
 
 void main() {
   runApp(TaskApp());
 }
 
 class TaskApp extends StatelessWidget {
-  TaskApp({super.key});
-
-  final List<Map<String, dynamic>> tasksJson = [
-    {"title": "Boodschappen doen", "done": false},
-    {"title": "Huiswerk maken", "done": true},
-    {"title": "Flutter oefenen", "done": false},
-  ];
-
-  final task = Task(id: "1", title: "Leren van mijn fouten", done: false);
+  const TaskApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    print("Test1: " + tasksJson[0]["titel"]);
-    
-    print("Test2: " + task.titel);
-
     return const MaterialApp(
       home: TaskHomePage(),
     );
