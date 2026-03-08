641863bf7dc1771d430613433caeef38254516e2
markvanmeerten <markvanmeerten@gmail.com>
2026-03-08 18:55:50 +0100

    wrong key example

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:todo/pages/task_home_page.dart';
+import 'package:todo/models/task.dart';
 
 void main() {
-  runApp(const TaskApp());
+  runApp(TaskApp());
 }
 
 class TaskApp extends StatelessWidget {
-  const TaskApp({super.key});
+  TaskApp({super.key});
+
+  final List<Map<String, dynamic>> tasksJson = [
+    {"title": "Boodschappen doen", "done": false},
+    {"title": "Huiswerk maken", "done": true},
+    {"title": "Flutter oefenen", "done": false},
+  ];
+
+  final task = Task(id: "1", title: "Leren van mijn fouten", done: false);
 
   @override
   Widget build(BuildContext context) {
+
+    print("Test1: " + tasksJson[0]["titel"]);
+    
+    print("Test2: " + task.titel);
+
     return const MaterialApp(
       home: TaskHomePage(),
     );
   }
 }
-
-
-
-
---
+++ lib/models/task.dart
@@ -0,0 +1,11 @@
+class Task {
+  final String id;
+  final String title;
+  final bool done;
+  
+  Task({
+    required this.id,
+    required this.title,
+    required this.done,
+  });
+}
